40ac3d37dfad197d635907e39af266930a9bfdbe
Noelle <ferrari.noelle@gmail.com>
2022-07-05 22:50:12 -0700

    Add color change on mouseover

--- sketchpad.js
+++ sketchpad.js
@@ -8,12 +8,28 @@ function createCanvas() {
         for(let x = 0; x < 16; x++) {
             const grid = document.createElement('div')
             grid.classList.add('grid')
+            grid.setAttribute('id', 'grid')
             row.appendChild(grid)
         }
-        // const grid = document.createElement('div')
-        // grid.classList.add('grid')
-        // container.appendChild(grid)
     }    
 }
 
 createCanvas()
+
+const grid = document.querySelectorAll('#grid')
+
+// function colorChange(itm) {
+//     if(itm.classList.contains('shaded')) {
+//         itm.classList.remove('shaded')
+//     } else {
+//         itm.classList.add('shaded')
+//     }
+
+// }
+
+grid.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+        console.log('change')
+        square.classList.toggle('shaded')
+    })
+})
